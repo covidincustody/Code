ab33d421b80b233eaf3281cf92537a9a275e6e4d
Covid In-Custody Project <117561631+covidincustody@users.noreply.github.com>
2023-02-16 21:07:14 -0500

    Update alameda_utils.py

--- Data_auto_collection/alameda/alameda_utils.py
+++ Data_auto_collection/alameda/alameda_utils.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -178,4 +172,3 @@ def single_column_obtain(filepath,column_name):
             else:
                 return row[16]
             
-
