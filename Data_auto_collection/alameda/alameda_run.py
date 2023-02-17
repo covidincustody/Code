e29af90fb77faa2e69f019e890633f452f8e8314
Covid In-Custody Project <117561631+covidincustody@users.noreply.github.com>
2023-02-16 21:08:52 -0500

    Update alameda_run.py

--- Data_auto_collection/alameda/alameda_run.py
+++ Data_auto_collection/alameda/alameda_run.py
@@ -1,5 +1,7 @@
 
 from alameda_utils import *
 
-alameda_auto_collection()
+filepath = r'C:\Users\gemini\Desktop\Data_collection_Santa-Rita.csv' #Change to your own filepath
+alameda_auto_collection(filepath)
+single_column_obtain(filepath,'Resolved Cases in Custody (Incarcerated population, current)') #Can change the column name
 
