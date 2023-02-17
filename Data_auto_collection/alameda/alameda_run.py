a35c28b33f51bae9fdc1af9ef4a572b30d853b0f
Covid In-Custody Project <117561631+covidincustody@users.noreply.github.com>
2023-02-16 22:15:27 -0500

    Update alameda_run.py

--- Data_auto_collection/alameda/alameda_run.py
+++ Data_auto_collection/alameda/alameda_run.py
@@ -1,6 +1,7 @@
 
 from alameda_utils import *
 
+url = 'https://content.govdelivery.com/accounts/ACSO/widgets/ACSO_WIDGET_2/0.json'#Ajax request url for Santa Rita jail
 filepath = r'C:\Users\gemini\Desktop\Data_collection_Santa-Rita.csv' #Change to your own filepath
 alameda_auto_collection(filepath)
 
