d5d2fefd447038ba5fbf8fb0cf21d6f3c5a7cd82
Covid In-Custody Project <117561631+covidincustody@users.noreply.github.com>
2023-03-14 21:14:43 -0400

    Update sf_run.py

--- Data_auto_collection/san_francisco/sf_run.py
+++ Data_auto_collection/san_francisco/sf_run.py
@@ -2,10 +2,11 @@
 from sf_utils import *
 
 address=r'C:\Users\gemini\Desktop\Data_collection_SF.csv' ###please change your local address right here
+url='https://www.sfsheriff.com/covid-19-jail-community-programs-sfso-staff-data' 
 
 if os.path.exists(address):
     with open(address,mode='r',encoding='utf-8') as ff:    
         print("File exist")
 else:
-    san_franciso_auto().to_csv(address,index=False)
+    san_franciso_auto(url).to_csv(address,index=False)
 
