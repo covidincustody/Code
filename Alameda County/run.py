d3317727e67b30d90b9ce0ffb6d19a097df352d7
Covid In-Custody Project <117561631+covidincustody@users.noreply.github.com>
2023-03-23 00:43:39 -0700

    Update and rename Data_auto_collection/alameda/alameda_run.py to Alameda County/run.py

---
+++ Alameda County/run.py
@@ -0,0 +1,7 @@
+
+from utils import *
+
+url = 'https://content.govdelivery.com/accounts/ACSO/widgets/ACSO_WIDGET_2/0.json' # Ajax request url for Santa Rita jail - link where COVID-19 data is updated by the sheriff's office
+filepath = r'C:\Users\gemini\Desktop\Data_collection_Santa-Rita.csv' # Change to your own filepath
+alameda_auto_collection(filepath)
+
